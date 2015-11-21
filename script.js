d0f57009e62128212e93783391401810137f7f74
yesaultsevum <yesaultsevum@gmail.com>
2015-11-21 08:47:41 +0200

    jslint

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 (function(){
+'use strict';
 
 var arr = new Array(9);
 
@@ -9,24 +10,24 @@ $('.cell').on('click', function(){
    var self = $(this);
    var id = self.attr('id');
    if(arr[id] !== 'player' && arr[id] !== 'ai'){
-      Player_turn(self, id);  
+      player_turn(self, id);
       if(checkArr(arr)){
          setTimeout(function (){
-           Ai_turn();
-         }, 200)
-      } 
+           ai_turn();
+         }, 200);
+      }
    }else{
       return;
-   } 
+   }
 });
 
-function Player_turn(self, id){
-   arr[id] = 'player'
+function player_turn(self, id){
+   arr[id] = 'player';
    $('#'+ id +'').addClass('player');
    checkEnd();
 }
 
-function Ai_turn(){
+function ai_turn(){
   var random = Math.floor(Math.random()*9);
    if(arr[random] == undefined){
      arr[random] = 'ai';
@@ -34,13 +35,14 @@ function Ai_turn(){
      checkEnd();
    }
    else{
-      return Ai_turn();
-   };
+      return ai_turn();
+   }
 }
 
 function checkArr(array){
-   for(var i = 0; i<array.length; i++){
-       var item = array[i];
+   var i, item;
+   for(i = 0; i<array.length; i+= 1){
+       item = array[i];
        if(item == undefined){
          return true;
        }else if(i < 9){
@@ -50,17 +52,23 @@ function checkArr(array){
 }
 
 function checkEnd() {
-  if (arr[0]=='ai' && arr[1]=='ai' && arr[2]=='ai' || arr[3]=='ai' && arr[4]=='ai' && arr[5]=='ai' || arr[6]=='ai' && arr[7]=='ai' && arr[8]=='ai') alert('you lose'), lose++, $('.lose span').text(lose), arr = new Array(9), $('.cell').removeClass('ai player');
-  if (arr[0]=='ai' && arr[3]=='ai' && arr[6]=='ai' || arr[1]=='ai' && arr[4]=='ai' && arr[7]=='ai' || arr[2]=='ai' && arr[5]=='ai' && arr[8]=='ai') alert('you lose'), lose++, $('.lose span').text(lose), arr = new Array(9), $('.cell').removeClass('ai player');
-  if (arr[0]=='ai' && arr[4]=='ai' && arr[8]=='ai' || arr[2]=='ai' && arr[4]=='ai' && arr[6]=='ai') alert('you lose'), lose++, $('.lose span').text(lose), arr = new Array(9), $('.cell').removeClass('ai player');
-  if (arr[0]=='player' && arr[1]=='player' && arr[2]=='player' || arr[3]=='player' && arr[4]=='player' && arr[5]=='player' || arr[6]=='player' && arr[7]=='player' && arr[8]=='player') alert('you win'), win++, $('.win span').text(win), arr = new Array(9), $('.cell').removeClass('ai player');
-  if (arr[0]=='player' && arr[3]=='player' && arr[6]=='player' || arr[1]=='player' && arr[4]=='player' && arr[7]=='player' || arr[2]=='player' && arr[5]=='player' && arr[8]=='player') alert('you win'), win++, $('.win span').text(win), arr = new Array(9), $('.cell').removeClass('ai player');
-  if (arr[0]=='player' && arr[4]=='player' && arr[8]=='player' || arr[2]=='player' && arr[4]=='player' && arr[6]=='player') alert('you win'), win++, $('.win span').text(win), arr = new Array(9), $('.cell').removeClass('ai player');
-  if (arr[0] && arr[1] && arr[2] && arr[3] && arr[4] && arr[5] && arr[6] && arr[7] && arr[8]) alert('draw'), arr = new Array(9), $('.cell').removeClass('ai player');
+  if (arr[0] === 'ai' && arr[1] === 'ai' && arr[2] == 'ai' || arr[3] == 'ai' && arr[4] == 'ai' && arr[5] == 'ai' || arr[6] == 'ai' && arr[7] == 'ai' && arr[8] == 'ai')
+     {alert('you lose'); lose++; $('.lose span').text(lose); arr = new Array(9); $('.cell').removeClass('ai player');}
+  if (arr[0] === 'ai' && arr[3] === 'ai' && arr[6] == 'ai' || arr[1] == 'ai' && arr[4] == 'ai' && arr[7] == 'ai' || arr[2] == 'ai' && arr[5] == 'ai' && arr[8] == 'ai')
+     {alert('you lose'); lose++; $('.lose span').text(lose); arr = new Array(9); $('.cell').removeClass('ai player');}
+  if (arr[0] === 'ai' && arr[4] === 'ai' && arr[8] == 'ai' || arr[2] == 'ai' && arr[4] == 'ai' && arr[6] == 'ai')
+     {alert('you lose'); lose++; $('.lose span').text(lose); arr = new Array(9); $('.cell').removeClass('ai player');}
+  if (arr[0] === 'player' && arr[1] === 'player' && arr[2] == 'player' || arr[3] == 'player' && arr[4] == 'player' && arr[5] == 'player' || arr[6] == 'player' && arr[7] == 'player' && arr[8] == 'player')
+     {alert('you win'); win++; $('.win span').text(win); arr = new Array(9); $('.cell').removeClass('ai player');}
+  if (arr[0] === 'player' && arr[3] === 'player' && arr[6] == 'player' || arr[1] == 'player' && arr[4] == 'player' && arr[7] == 'player' || arr[2] == 'player' && arr[5] == 'player' && arr[8] == 'player')
+     {alert('you win'); win++; $('.win span').text(win); arr = new Array(9); $('.cell').removeClass('ai player');}
+  if (arr[0] === 'player' && arr[4] === 'player' && arr[8] == 'player' || arr[2] == 'player' && arr[4] == 'player' && arr[6] == 'player')
+     {alert('you win'); win++; $('.win span').text(win); arr = new Array(9); $('.cell').removeClass('ai player');}
+  if (arr[0] && arr[1] && arr[2] && arr[3] && arr[4] && arr[5] && arr[6] && arr[7] && arr[8])
+     {alert('draw'); arr = new Array(9); $('.cell').removeClass('ai player');}
 }
 
-})()
-
+})();
 
 
 
